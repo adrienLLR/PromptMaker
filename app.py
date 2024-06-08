d8de8ec2250f80ef31816b54c3ba42b34cbd5b01
Adrien <lelouer.adrien@gmail.com>
2024-06-09 01:39:45 +0200

    Add helpers and placeholder

--- app.py
+++ app.py
@@ -22,16 +22,15 @@ with st.expander("**:bulb: AI settings**", expanded=True):
     c1, c2 = st.columns(2)
 
     model = c1.selectbox("Model", options=["gpt-3.5-turbo", "gpt-3.5-turbo-instruct","gpt-4", "gpt-4-turbo"])
-    api_key = c2.text_input("OpenAI API key", type="password", placeholder="This will never be stored",
-                            help="If you do not have one, simply click on `Visualize prompt` above and copy paste the generated prompt into [ChatGPT]()")
+    api_key = c2.text_input("OpenAI API key", type="password", placeholder="This will never be stored")
     temperature = st.slider("Temperature", min_value=0.0, max_value=2.0, step=0.01, value=1.0,
                             help="Controls the “creativity” or randomness of the output. Higher temperatures (e.g., 0.7) result in more diverse and creative output (and potentially less coherent), while a lower temperature (e.g., 0.2) makes the output more deterministic and focused.")
     top_p = st.slider("Top p", min_value=0.0, max_value=1.0, step=0.01, value=1.0,
-                    help="")
+                    help="Consider only the possibilities that equal or exceed the top p value. Kind of a similar effect as the temperature")
     freq_penalty = st.slider("Frequency Penalty", min_value=-2.0, max_value=2.0, step=0.01, value=0.0,
-                        help="")
+                        help="Penalize tokens that were sampled too much prior to the current position")
     presence_penalty = st.slider("Presence Penalty", min_value=-2.0, max_value=2.0, step=0.01, value=0.0,
-                    help="")
+                    help="Penalize tokens that were at least sampled once prior to the current position")
 
 with st.expander("**:bookmark_tabs: Prompt Settings**", expanded=True):
     st.button('Add a prompt', on_click=increment_nb_prompts)
@@ -41,7 +40,10 @@ with st.expander("**:bookmark_tabs: Prompt Settings**", expanded=True):
 
 with st.expander("**:arrow_forward: Prompts**", expanded=True):
     for i in range(st.session_state.nb_prompts):
-        st.text_area(f'Prompt {i+1}', key=f'prompt_{i}')
+        if i == 0:
+            st.text_area(f'Prompt {i+1}', key=f'prompt_{i}', placeholder="To use the output of the prompt i in the following prompts, type [i].\nFor example : 'Add the following two numbers : [1] and [2]'")
+        else:
+            st.text_area(f'Prompt {i+1}', key=f'prompt_{i}')
     st.button("Generate answer", use_container_width=True, on_click=generate_answer, kwargs={"api_key":api_key, "model":model})
 
 # for i in range(st.session_state.nb_prompts):
